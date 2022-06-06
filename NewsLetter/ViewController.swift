66634f40f41314d842dc28c43be3ae7cf6914975
Minji Kim <minji_0801@naver.com>
2022-06-06 23:27:47 +0900

    Initial Commit

---
+++ NewsLetter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsLetter
+//
+//  Created by 김민지 on 2022/06/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
