5dd27d407727e45f78d43911c9fe36a712b46e70
Minji Kim <minji_0801@naver.com>
2022-06-12 22:00:00 +0900

    Modify NewsAPIKey file

---
+++ NewsLetter/Sources/NewsAPIKey.swift
@@ -0,0 +1,11 @@
+//
+//  NewsAPIKey.swift
+//  NewsLetter
+//
+//  Created by 김민지 on 2022/06/12.
+//
+
+struct NewsAPIKey {
+  static let id: String = "vG95SRrIYs7muxvV4t19"
+  static let secret: String = "nZkbphXlYx"
+}
